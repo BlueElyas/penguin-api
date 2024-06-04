26c984bd973ea9fded776af6311fd4e0b5b921a1
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-06-04 19:54:39 +0100

    Add Put, POST and DELETE requests

--- routers/penguins.js
+++ routers/penguins.js
@@ -22,3 +22,31 @@ penguinRouter.get("/", (req, res, next) => {
 penguinRouter.get("/:penguinId", (req, res, next) => {
   res.send(req.penguin);
 });
+
+penguinRouter.post("/", (req, res, next) => {
+  req.body.id = penguins.length + 1;
+  penguins.push(req.body);
+  res.status(201).send(req.body);
+});
+
+penguinRouter.put("/:penguinId", (req, res, next) => {
+  // Get ID from URL parameter
+  const index = penguins.findIndex((penguin) => penguin.id === req.penguin.id);
+  if (index >= 0) {
+    penguins[index] = { id: req.penguin.id, ...req.body };
+    res.send(penguins[index]);
+  } else {
+    res.status(404).send("Penguin not found!");
+  }
+});
+
+penguinRouter.delete("/:penguinId", (req, res, next) => {
+  if (req.penguin.id >= 0) {
+    const filteredArr = penguins.filter(
+      (penguin) => penguin.id !== req.penguin.id
+    );
+    res.send(filteredArr);
+  } else {
+    res.status(404).send("Delete failed");
+  }
+});
